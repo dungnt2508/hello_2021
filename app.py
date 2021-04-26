5862ab7322cd34a0b98f775d6beeba483cd3161f
Dung Nguyen <ntdung2508@gmail.com>
2021-04-26 17:25:29 +0700

    test push game

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from pusher import pusher
 app = Flask(__name__)
 
 pusher = pusher_client = pusher.Pusher(
-  app_id='1193552',
-  key='797cf18b328b54dede0e',
-  secret='eb6dcea8fba67a8e13d9',
-  cluster='ap1',
+  app_id='PUSHER_APP_ID',
+  key='PUSHER_APP_KEY',
+  secret='PUSHER_APP_SECRET',
+  cluster='PUSHER_APP_CLUSTER',
   ssl=True
 )
 
@@ -35,7 +35,6 @@ def pusher_authentication():
       }
     }
   )
-  print(auth)
   return json.dumps(auth)
 
 if __name__ == '__main__':
